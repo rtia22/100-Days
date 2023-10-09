c24c9783ce7534789163eccca8ca1e87a5a5a401
Rahmatia <147220961+rtia22@users.noreply.github.com>
2023-10-09 15:51:31 +0800

    Tipe Data String

---
+++ Day001.java
@@ -0,0 +1,9 @@
+public class Day001 {
+	public static void main(String[] args) {
+		String nama = "Rahmatia";
+		String nim = "D0222304";
+		
+		System.out.println("Nama : " + nama);
+		System.out.println("Nim : " + nim);
+	}
+}
