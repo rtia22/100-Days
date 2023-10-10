d364be464e144a926391a5a66408e9adced57805
Rahmatia <147220961+rtia22@users.noreply.github.com>
2023-10-10 20:05:11 +0800

    Add files via upload
    Aritmatika

---
+++ Day002.java
@@ -0,0 +1,12 @@
+
+public class Day002 {
+
+    public static void main(String[] args) {
+       int a = 9;
+       int b = 6;
+       
+       int hasil = a + ( b * b ) - ( a / b );
+        System.out.println("Hasil ARitmatika :" + hasil);
+       
+    }
+}
