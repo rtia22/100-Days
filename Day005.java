0d5828558bccccaa069a421f4399dcec3cd9103c
Rahmatia <147220961+rtia22@users.noreply.github.com>
2023-10-13 22:17:04 +0800

    if-else

---
+++ Day005.java
@@ -0,0 +1,10 @@
+public class Day005{
+    public static void main(String[] args) {
+    int nilai = 5;
+    if(nilai == 5){
+        System.out.println("Benar");
+    }else{
+        System.out.println("Salah");
+    }
+}
+}
