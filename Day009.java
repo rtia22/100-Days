796d0152b30b74e9da29c8088fb8ed1a818b957c
Rahmatia <147220961+rtia22@users.noreply.github.com>
2023-10-22 22:40:34 +0800

    Update Day009.java

--- Day009.java
+++ Day009.java
@@ -1,12 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Asus
- */
 public class Day009 {
-    
+
+    public static void main(String[] args) {
+        int n = 10; // Jumlah angka dalam deret
+        int sukuPertama = 2; // Suku pertama dalam deret
+        int selisih = 3; // Selisih antara setiap dua suku
+
+        System.out.println("Deret aritmatika dengan " + n + " angka:");
+        int i = 1;
+        while (i <= n) {
+            int suku_ke_i = sukuPertama + (i - 1) * selisih;
+            System.out.print(suku_ke_i + " ");
+            i++;
+        }
+    }
 }
