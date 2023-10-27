e73b1d3cdf2062a22f897b42e3fc5b4f32ad63fb
Rahmatia <147220961+rtia22@users.noreply.github.com>
2023-10-27 19:06:57 +0800

    Day019.java

--- Day019.java
+++ Day019.java
@@ -1,12 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Asus
- */
 public class Day019 {
-    
+
+    public static void main(String[] args) {
+        double[] data = {
+            91.35, 4.72, 26, 3.5, 8, 61, 65.36, 12, 5.6, 7.11,
+            27, 9.53, 549, 2.23, 17.31, 4.25, 2.13, 83, 7, 102.4,
+            53.21, 3.42, 0.21, 70.02, 819.4, 6173, 4.25, 19.8, 17.35, 5.768
+        };
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+        double sum = 0;
+        System.out.print("[");
+        for (int i = 0; i < data.length; i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(data[i]);
+
+            if (data[i] < min) {
+                min = data[i];
+            }
+            if (data[i] > max) {
+                max = data[i];
+            }
+
+            sum += data[i];
+        }
+        System.out.println("]");
+
+        double avg = sum / (double) data.length;
+
+        System.out.println("Min = " + min);
+        System.out.println("Max = " + max);
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + avg);
+
+    }
+
 }
