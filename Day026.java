359757d569c9f4d0be180e73ebbf3278d06f443a
Rahmatia <147220961+rtia22@users.noreply.github.com>
2023-11-03 21:27:19 +0800

    hitung luas dan keliling lingkaran

---
+++ Day026.java
@@ -0,0 +1,12 @@
+
+public class Day026 {
+    public static void main(String[] args) {
+        double jarijari = 6.5;
+        
+        double luas = 2 * Math.pow(jarijari, 2);//HITUNG LUAS
+        System.out.println("Luas Lingkaran : " + luas);
+        
+        double keliling = 2 * Math.PI * jarijari; //HITUNG KELILING
+        System.out.println("Keliling Lingkaran : " + keliling);
+    }
+}
