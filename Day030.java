5b2ef5d3e798acfba26644e8b3562416f5787265
Rahmatia <147220961+rtia22@users.noreply.github.com>
2023-11-07 20:37:42 +0800

    menentukan bilangan ganjil genap

---
+++ Day030.java
@@ -0,0 +1,14 @@
+
+public class Day030 {
+
+    public static void main(String[] args) {
+        for (int a = 1; a <= 10; a++) {
+            if (a % 2 == 1) {
+                System.out.println(a + " = Bilangan Ganjil");
+            }else {
+                System.out.println(a + " = Bilangan Genap");
+            }
+        }
+    }
+
+}
