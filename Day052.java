6e42f6f2faa9d4d99b86693357afaac1928ed08b
Rahmatia <147220961+rtia22@users.noreply.github.com>
2023-11-29 23:01:26 +0800

    Update Day052.java
    Mencari angkat tertentu dalam array

--- Day052.java
+++ Day052.java
@@ -1,12 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Asus
- */
+
+import java.util.Scanner;
+
 public class Day052 {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[50];
+        int arr_count, i, num;
+
+        System.out.print("Input jumlah elemen Array: ");
+        arr_count = sc.nextInt();
+
+        System.out.println("Input " + arr_count + " angka (dipisah dengan enter): ");
+        System.out.println();
+
+        //Simpan setiap angka yang diinput ke dalam array
+        for (i = 0; i < arr_count; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+
+        System.out.println();
+
+        System.out.print("Input angka yang akan dicari: ");
+        num = sc.nextInt();
+
+        //Proses pencarian Array
+        for (i = 0; i < arr_count; i++) {
+            if (arr[i] == num) {
+                System.out.print("Angka ditemukan pada index ke-" + i);
+                break;
+            }
+
+        }
+
+        if (i == arr_count) {
+            System.out.println("Angka tidak ditemukan");
+        }
+
+        System.out.println();
+    }
+
 }
