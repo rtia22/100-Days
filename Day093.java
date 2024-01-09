76432d6c2de7281758d1589352db4f10711d32d0
Rahmatia <147220961+rtia22@users.noreply.github.com>
2024-01-09 21:16:21 +0800

    Example penggunaan class random

---
+++ Day093.java
@@ -0,0 +1,11 @@
+
+import java.util.Random;
+
+
+public class Day093 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int angkaRandom = random.nextInt(10);
+        System.out.println("Angka random : " + angkaRandom);
+    }
+}
